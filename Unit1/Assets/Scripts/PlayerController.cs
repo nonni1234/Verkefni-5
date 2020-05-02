d7566e1e42cf9b17f4e8bc7d99dac06b0a57f823
nonni1234 <nonni1234567@gmail.com>
2020-05-02 14:22:51 +0000

    Unit 1

---
+++ Unit1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Private variables
+    private float speed = 20f;
+    private float turnSpeed = 55f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // Player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical"); 
+    }
+    void FixedUpdate()
+    {
+        // Move the car forward from Vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Move the car forward from Horizontal input
+        transform.Rotate(Vector3.up,turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
